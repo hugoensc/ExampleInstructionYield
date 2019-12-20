9cf6cb60ba0b7078314ef70137c2f0e58ada4c79
Hugo Santamaria <hugo.santamaria@alianzacorp.mx>
2019-12-20 12:33:46 -0600

    ExampleInstructionYield

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExampleInstructionYield
+{
+    class Program
+    {
+        static IEnumerable<int> Range(int from, int to)
+        {
+            for (int i = from; i < to; i++)
+            {
+                yield return i;
+            }
+            yield break;
+        }
+
+        static void Main(string[] args)
+        {
+            foreach (int x in Range(-10, 10))
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
